58a999532d5a5938c49df2e069cf3eded7b4a2bd
Mujgan Aydin Oymaci <mujganaydin0732@gmail.com>
2024-04-17 00:54:31 +0300

    .

--- src/test/java/com/library/pages/LoginPage.java
+++ src/test/java/com/library/pages/LoginPage.java
@@ -2,9 +2,11 @@ package com.library.pages;
 
 import com.library.utility.ConfigurationReader;
 import com.library.utility.Driver;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
 
 
 public class LoginPage {
@@ -31,6 +33,8 @@ public class LoginPage {
         emailBox.sendKeys(username);
         passwordBox.sendKeys(password);
         loginButton.click();
+
+
 
     }
 
